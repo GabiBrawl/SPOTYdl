9d43729e8e70f1f607676b219ad4e59efb469d02
GabiBrawl <85069997+GabiBrawl@users.noreply.github.com>
2022-02-27 22:16:28 +0000

    Update clnup.bat
    v1.1

--- server/clnup.bat
+++ server/clnup.bat
@@ -5,7 +5,7 @@ mode con: cols=70 lines=15
 
 
 :confirmation
-echo You're going to delete all the files in the .cache folder.
+echo You're going to delete all the setup files.
 echo.
 set /p confirm=y/n^>
 if %confirm%==y goto cleanup
@@ -14,12 +14,20 @@ if %confirm%==n goto failure
 
 :cleanup
 echo Cleaning up your system...
-del .cache /y
+del "SPOTYdl - SETUP" /y
+del "7z.dll" /y
+del "7z.exe" /y
+del "7-zip.dll" /y
+del "BITS_DN.cmd" /y
+del "bitsadmin.exe" /y
+del "ffmpeg-n5.0-latest-win64-gpl-5.0.zip" /y
+del "python-3.10.2-amd64.exe" /y
 del ClnUp.bat /y
 goto success
 
 
 :failure
+cls
 echo Cleanup not done.
 echo by GabiBrawl
 echo.
@@ -28,6 +36,7 @@ pause >nul
 
 
 :success
+cls
 echo Your system is now cleared up!
 echo Now you can start downloading your spotify music!
 echo by GabiBrawl
