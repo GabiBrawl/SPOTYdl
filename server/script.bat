34f4d336020e08002e5cfaabb2ec769dc16bd057
GabiBrawl <85069997+GabiBrawl@users.noreply.github.com>
2022-04-04 16:41:15 +0100

    Good news!! V1.6.1 is out!!
    Completely (I guess) debugged!!

--- server/script.bat
+++ server/script.bat
@@ -1,30 +1,44 @@
 @echo off
-mode con: cols=72 lines=15
-color 07
-title SPOTYdl
-:prepreset
-if exist "setup.bat" (del "setup.bat" && goto preset) else (goto preset)
-:preset
-if exist "Downloads" (cls && goto set) else (md Downloads && goto set)
-:set
-cls
+::---------------------------------------
 set wm=Normal
-set ver=1.4.5
-set channel=Public
+set ver=1.6
+set channel=Beta
 ::set edition= [WebUI or non graphical]
-goto output-format
+::---------------------------------------
+title SPOTYdl
+color 07
+::---------------------------------------
+if exist setup.bat (
+	del setup.bat
+	if exist Downloads (
+		goto aoff
+	) else (
+		md Downloads
+		goto aoff
+	)
+) else (
+	if exist Downloads (
+		cls
+		goto aoff
+	) else (
+		md Downloads
+		goto aoff
+	)
+)
 
 
-
-:output-format
-if exist "s.ver" (del s.ver)
-mode con: cols=72 lines=15
+:aoff ::audio output file format
+title SPOTYdl
+if exist s.ver (del s.ver)
+if exist news.temp (del news.temp)
+mode con: cols=72 lines=18
+color 07
 set of=
 cls
-echo  Working mode: %wm%; for detailed info type /wm
+echo  Working mode: %wm%; for detailed info type "-wm"
 echo.
 echo.
-echo  Available formats:
+echo  Available audio file formats:
 echo   1) mp3                 4) opus
 echo   2) m4a		 5) ogg
 echo   3) flac		 6) wav
@@ -32,92 +46,60 @@ echo.
 echo  Other options:
 echo   a) Help                b) Toggle Working Mode
 echo   c) About               d) Version
+echo   e) News                f) Changelog
 echo.
-echo Input the number that corresponds to your choice.
-SET of=%~1
-SET char=%of:~0,1%
-set /p of=^>
-if "%of%"=="/help" (set hlp=output-format && goto help)
-if "%of%"=="a" (set hlp=output-format && goto help)
+echo  Input the number that corresponds to your choice.
+set /p of=^>^> 
 if "%of%"=="1" (set format="mp3" && goto set-link)
 if "%of%"=="2" (set format="m4a" && goto set-link)
 if "%of%"=="3" (set format="flac" && goto set-link)
 if "%of%"=="4" (set format="opus" && goto set-link)
 if "%of%"=="5" (set format="ogg" && goto set-link)
 if "%of%"=="6" (set format="wav" && goto set-link)
-if "%of%"=="c" (set goto=output-format&&goto about)
-if "%of%"=="b" (if %wm%==Normal (set wm=Multiple&& goto output-format) else (set wm=Normal&& goto output-format))
+if "%of%"=="a" (set hlp=aoff && goto help)
+if "%of%"=="b" (if %wm%==Normal (set wm=Multiple&& goto aoff) else (set wm=Normal&& goto aoff))
+if "%of%"=="c" (set goto=aoff&&goto about)
 if "%of%"=="d" (goto dvfs)
-if "%of%"=="mp3" (set format="mp3" && goto set-link)
-if "%of%"=="m4a" (set format="m4a" && goto set-link)
-if "%of%"=="flac" (set format="flac" && goto set-link)
-if "%of%"=="opus" (set format="opus" && goto set-link)
-if "%of%"=="ogg" (set format="ogg" && goto set-link)
-if "%of%"=="wav" (set format="wav" && goto set-link)
-if "%of%"=="/wm" (set goto=output-format && goto set_working_mode)
-:invalid
-cls
+if "%of%"=="e" (goto news)
+if "%of%"=="f" (goto changelog)
+if "%of%"=="-wm" (set goto=aoff && goto set_working_mode)
 echo Sorry, but the value you entered is invalid. Try again!
 timeout /t 3 >nul
-goto output-format
+goto aoff
 
 
 :set-link
-if exist s.ver (del "s.ver")
-mode con: cols=85 lines=7
+if exist s.ver (del s.ver)
+mode con: cols=69 lines=9
+set link=
 cls
-echo  Working mode: %wm%, to change it type /wm
-echo  Output format: %format%, to change it type /b
-echo  If you wanna download all songs on a txt file, use /multi
 echo.
-echo  Now paste the link to your song/playlist or simply the song name!! :D
-set /p link=Now: 
-if "%link%"=="/b" goto output-format
-if "%link%"=="/wm" (set goto=set-link && goto set_working_mode)
-if "%link%"=="/multi" (goto multi)
+echo   Working mode: %wm%, to change it type "-wm"
+echo   Output format: %format%, to change it type "-b"
+echo   If you wanna download all songs on a txt file, use "-txt"
+echo.
+echo   Paste the link to your song/playlist or simply the song name!! :D
+set /p link=^>^>
+if "%link%"=="-b" goto aoff
+if "%link%"=="-wm" (set goto=set-link && goto set_working_mode)
+if "%link%"=="-txt" (goto multi)
+if not defined link goto blank_invalid
 :download
 cls
-spotdl %link% --output-format %format% --output .\Downloads\
-if link=="" goto blank_invalid
-if %errorlevel% == 0 goto success
+spotdl "%link%" --output-format %format% --output .\Downloads\
+if %errorlevel% == 0 goto clnup
 if %errorlevel% == 1 goto error1
 ::if %errorlevel% == 2 goto success
-pause
 :blank_invalid
-echo  You can't leave this blank.
-echo  Try again!
+echo   You can't leave this field blank. Try again!
 timeout /t 3 >nul
 goto set-link
 
 
-:set_working_mode
-set swm=""
-cls
-echo  Chose BEditor working mode
-::by GabiBrawl
-echo  1) Normal mode
-echo  2) Multiple mode
-echo  3) Help
-echo.
-echo  Input the number that corresponds to your choice.
-set /p swm= $ 
-if "%swm%"=="1" (set wm=Normal&&goto %goto%)
-if "%swm%"=="2" (set wm=Multiple&&goto %goto%)
-if "%swm%"=="3" (goto help_wm)
-::----------------------------------------------------------------------------------------------------
-if "%swm%"=="Normal" (set wm=Normal&&goto %goto%)
-if "%swm%"=="Multiple" (set wm=Multiple&&goto %goto%)
-if "%swm%"=="help" (goto help_wm)
-:invalid
-cls
-echo  The value you entered is invalid. Try again!
-timeout /t 3 >nul
-goto set_working_mode
-
-
 :multi
-mode con: cols=72 lines=10
+mode con: cols=72 lines=13
 cls
+echo.
 echo                      Multiple songs downloader mode
 echo.
 echo.
@@ -126,41 +108,67 @@ echo   Tip: drag and drop your file here to auto type it's location.
 echo   Note: To download all songs from multiple artists, paste the spotify
 echo  artists' links in the text file and not the artist name.
 echo.
-echo  To go back, use /b
-set /p txt=^>
-if %txt%==/b goto set-link
-for /F "usebackq tokens=*" %%A in (%txt%) do spotdl %%A --output-format %format%
-if %errorlevel% == 0 goto success
+echo  To go back, use "-b"
+set /p txt=^>^>
+if not defined txt goto ibtxt
+if %txt%==-b goto set-link
+for /F "usebackq tokens=*" %%A in (%txt%) do spotdl "%%A" --output-format %format% --output .\Downloads\
+if %errorlevel% == 0 goto clnup
 if %errorlevel% == 1 goto error1
 echo This is yet bugged lol
 pause
-goto output-format
+goto aoff
+:ibtxt
+echo   You can't leave this field blank. Try again!
+timeout /t 3 >nul
+goto multi
+
+
+:set_working_mode
+mode con: cols=59 lines=10
+set swm=""
+cls
+echo.
+echo                Change BEditor working mode
+echo.
+::by GabiBrawl
+echo   1) Normal mode, will download music once and then exit.
+echo   2) Multiple mode, won't exit when you download a music.
+echo.
+echo   Input the value that corresponds to your choice.
+set /p swm=^>^> 
+if "%swm%"=="1" (set wm=Normal&&goto %goto%)
+if "%swm%"=="2" (set wm=Multiple&&goto %goto%)
+echo        The value you entered is invalid. Try again!
+timeout /t 3 >nul
+goto set_working_mode
 
 
 :dvfs
-if %channel%==Public (
+if %channel%==Stable (
 	if exist s.ver (
 		del s.ver
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver">nul
 		goto version
 	) else (
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver">nul
 		goto version
 	)
 ) else (
 	if exist s.ver (
 		del s.ver
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver">nul
 		goto version
 	) else (
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver">nul
 		goto version
 	)
 )
 
+
 :version
 mode con: cols=85 lines=15
-set swm=""
+set swm=
 set sver=no connection
 set /p sver=<s.ver
 cls
@@ -171,53 +179,63 @@ echo  Update channel: %channel%
 ::echo  Edition: %edition%
 echo.
 ::by GabiBrawl
-echo  1) Install updates
+if not "%sver%"=="no connection" (
+if not "%sver%"=="%ver%" (
+	echo  1^) Install updates
+) else (
+	echo  1^) Reinstall SPOTYdl
+)
+) else (
+	echo  1^) No connection
+)
 echo  2) Change update channel
 echo  3) Help
 echo  4) Go back
 echo  5) Reload server version
 echo.
-echo  Input the number that corresponds to your choice.
-echo  NOTE: you don't need to install updates if server and installed versions are equal 
-set /p swm= $ 
+echo  Input the number that corresponds to your choice. 
+set /p swm=^>^> 
+if not defined swm goto biv
 if "%swm%"=="1" (goto donw_and_inst)
 if "%swm%"=="2" (goto update_chnl)
 if "%swm%"=="3" (goto help_v)
-if "%swm%"=="4" (goto output-format)
-if "%swm%"=="5" (if %channel%==Public (
+if "%swm%"=="4" (goto aoff)
+if "%swm%"=="5" (if %channel%==Stable (
 	if exist s.ver (
 		del s.ver
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver">nul
 		goto version
 	) else (
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/s.ver -Outfile s.ver">nul
 		goto version
 	)
 ) else (
 	if exist s.ver (
 		del s.ver
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver">nul
 		goto version
 	) else (
-		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver"
+		powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bs.ver -Outfile s.ver">nul
 		goto version
 	)
 )
 )
-:invalid
-cls
 echo  The value you entered is invalid. Try again!
 timeout /t 3 >nul
 goto version
-
+:biv
+echo  You can't leave this field blank. Try again!
+timeout /t 3 >nul
+goto version
 
 
 :donw_and_inst
 cls
 echo  Downloading and installing the latest version of SPOTYdl.
 echo  Don't close this window.
-if %channel%==Public (powershell -command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/script.bat -Outfile SPOTYdl.temp") else (powershell -command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bscript.bat -Outfile SPOTYdl.temp")
+if %channel%==Stable (powershell -command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/script.bat -Outfile SPOTYdl.temp" >nul) else (powershell -command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/bscript.bat -Outfile SPOTYdl.temp" >nul)
 if %errorlevel%==1 goto fail_github
+echo.
 echo @echo off>>.\setup.bat
 echo title Finishing up>>.\setup.bat
 echo del SPOTYdl.bat>>.\setup.bat
@@ -228,66 +246,93 @@ start setup.bat
 exit
 
 
-
-:fail_github
+:update_chnl
+mode con: cols=60 lines=12
 cls
 echo.
-echo  Could not establish connection with GitHub.
-echo  Please try again later!
-timeout /t 4 > nul
-goto version
-
-
-:update_chnl
-cls
-echo  How does this work?
-echo  When you change your update channel, you will install
+echo   How does this work?
+echo   When you change your update channel, you will install
 echo  the latest update within the channel you chose.
+echo   Not ready to change? Just use [Enter] and you'll go back.
 echo.
 echo  Currently available channels:
-echo  a) Public (the most stable)
+echo  a) Stable
 echo  b) Beta
-echo  c) go back
 set /p chnl=^>
-if %chnl%==a (set channel=Public && goto donw_and_inst)
-if %chnl%==b (set channel=Beta && goto donw_and_inst)
-if %chnl%==c (goto version)
-:invalid
+if "%chnl%"=="a" (set channel=Stable && goto donw_and_inst)
+if "%chnl%"=="b" (set channel=Beta && goto donw_and_inst)
+if "%chnl%"=="c" (goto version)
+if not defined chnl goto version
+
+
+:news
+title SPOTYdl - NEWS
+mode con: cols=66 lines=35
+color 4e
+if exist news.temp (
+	del news.temp
+	powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/news.temp -Outfile news.temp" >nul
+	if %errorlevel%==1 goto fail_github
+	goto news_read
+) else (
+	powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/news.temp -Outfile news.temp" >nul
+	if %errorlevel%==1 goto fail_github
+	goto news_read
+)
+:news_read
+if not exist news.temp goto fail_github
 cls
-echo  The value you entered is invalid. Try again!
-timeout /t 3 >nul
-goto set_working_mode
+echo  ------------------------------NEWS------------------------------
+type news.temp |More
+echo.
+echo.
+echo       Congrats! You've read all available news for today!
+echo          Press any key to go back to the main menu.
+pause>nul
+goto aoff
 
-
-
-
+:clnup
+cls
+echo.
+echo Cleaning things up...
+del ".\Downloads\.spotdl-cache" >nul
 :success
-cls
 echo Song/Playlist was successfuly downloaded!
-powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'SPOTYdl', 'Your download is completed!', [System.Windows.Forms.ToolTipIcon]::None)}"
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'SPOTYdl', 'Your download is completed!', [System.Windows.Forms.ToolTipIcon]::None)}">nul
 echo.
 if %wm%==Normal (echo Press any key to exit) else (echo Press any key to continue)
 pause >nul
 if %wm%==Normal (exit) else (goto set-link)
-::set action="exit"
-::goto cleanup
-
 
 
 :error1
 cls
 echo The music you tried to download was not found. Try again.
-powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Error; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'SPOTYdl', 'Your download failed...', [System.Windows.Forms.ToolTipIcon]::None)}"
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Error; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'SPOTYdl', 'Your download failed...', [System.Windows.Forms.ToolTipIcon]::None)}">nul
 timeout /t 5 >nul
 goto set-link
-::set action="goto set-link"
-::goto cleanup
 
 
+:changelog
+if exist changelog.txt (del changelog.txt)
+echo|set /p "=Downloading the changelog now... " <nul
+powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/GabiBrawl/SPOTYdl/main/server/changelog.txt -Outfile changelog.txt" >nul
+if %errorlevel%==1 goto fail_github
+echo Done!
+timeout /t 3 >nul
+start changelog.txt
+goto aoff
 
-::cleanup
-::del ".spotdl-cache" >nul
-::%action%
+
+:fail_github
+color 07
+mode con: cols=45 lines=4
+cls
+echo.
+echo  Could not establish connection with GitHub.
+echo  Please try again later!
+timeout /t 4 > nul
+goto aoff
 
 
 :help
@@ -302,37 +347,16 @@ pause >nul
 goto %hlp%
 
 
-
-:help_wm
-mode con: cols=55 lines=14
-cls
-echo                -WorkingMode HELP file-
-echo.
-echo.
-echo    Normal mode will make SPOTYdl download music
-echo   only once, then exit.
-echo    Multiple mode will let you download music
-echo   multiple times. Search for one music, and it'll
-echo   prompt you again for music location without exiting!
-echo.
-echo   Press any key to go back... && pause >nul && goto set_working_mode
-
-
-
 :help_v
-mode con: cols=60 lines=14
+mode con: cols=62 lines=8
 cls
-echo                -Version HELP file-
+echo                    -Version HELP file-
 echo.
 echo.
 echo   Here you cand check and download new updates for SPOTYdl.
-echo  Just check if server and installed versions are equal. If
-echo  not, you may want to update.
-echo   The updating process takes no more than some seconds.
+echo   The updating process takes no more than some milliseconds.
 echo.
 echo   Press any key to go back... && pause >nul && goto version
-
-
 
 
 :about
@@ -349,9 +373,6 @@ echo  installed it, and it worked nicely, but it's not motivating to have to ope
 echo  download a song. So I created an automated script to chose format and song link. Working great, but
 echo  once again, I wanna publish it. So I created a setup to download and setup everything automaticaly,
 echo  so we don't need to go thru all that painful process of installation, nor usage.
-echo.
-echo  Soon a WebUI version will be out. SPOTYdl is now implemented with an update system, so you can
-echo  upgrade anytime. Check out "Version" for more info! :D
 echo  And here we are!
 echo.
 echo                                      Welcome to SPOTYdl V%ver%!
